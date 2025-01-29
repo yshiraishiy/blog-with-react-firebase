0eb2838b6694f5839870271b99fdfaf878f3e791
yshiraishiy <yuya0505soccer@gmail.com>
2025-01-29 16:22:54 +0900

    ✨ホームページの雛形を実装

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,23 @@
 import React from "react";
 
+import "./Home.css";
+
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="homePage">
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+        <div className="postTextContainer">
+          今はReactの学習中です。 これから頑張って活躍していきたいです。
+          よろしくお願いします。
+        </div>
+        <div className="nameAndDeleteButton">
+          <h3>@白石裕也</h3>
+          <button>削除</button>
+        </div>
+      </div>
+    </div>
+  );
 };
