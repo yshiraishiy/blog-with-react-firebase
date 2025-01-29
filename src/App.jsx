1344e6ec13a8faad97aad67703c3983929a6bf94
yshiraishiy <yuya0505soccer@gmail.com>
2025-01-29 15:06:51 +0900

    ✨ログインとログアウトを着替える

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <Router>
-      <Navbar />
+      <Navbar isAuth={isAuth} />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/createpost" element={<CreatePost />}></Route>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse, faFilePen, faArrowRightToBracket } from "@fortawesome/free-solid-svg-icons";
+import {
+  faHouse,
+  faFilePen,
+  faArrowRightToBracket,
+} from "@fortawesome/free-solid-svg-icons";
 
 import "./Navbar.css";
 
-export const Navbar = () => {
+export const Navbar = ({ isAuth }) => {
   return (
     <nav>
-      <Link to="/"><FontAwesomeIcon icon={faHouse} />ホーム</Link>
-      <Link to="/createpost"><FontAwesomeIcon icon={faFilePen} />記事投稿</Link>
-      <Link to="/login"><FontAwesomeIcon icon={faArrowRightToBracket} />ログイン</Link>
+      <Link to="/">
+        <FontAwesomeIcon icon={faHouse} />
+        ホーム
+      </Link>
+      <Link to="/createpost">
+        <FontAwesomeIcon icon={faFilePen} />
+        記事投稿
+      </Link>
+      {!isAuth ? (
+        <Link to="/login">
+          <FontAwesomeIcon icon={faArrowRightToBracket} />
+          ログイン
+        </Link>
+      ) : (
+        <Link to="/logout">
+          <FontAwesomeIcon icon={faArrowRightToBracket} />
+          ログアウト
+        </Link>
+      )}
     </nav>
   );
 };
