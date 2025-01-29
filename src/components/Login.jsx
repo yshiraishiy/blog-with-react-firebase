7f3d82c31d846cd29e0422640f46205ce74e2853
yshiraishiy <yuya0505soccer@gmail.com>
2025-01-29 14:31:08 +0900

    ✨Googleでログイン機能の実装

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,22 @@
+import { signInWithPopup } from "firebase/auth";
 import React from "react";
+import { auth, provider } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
-export const Login = () => {
-  return <div>Login</div>;
+export const Login = ({ setIsAuth }) => {
+  const navigate = useNavigate();
+  const loginInWithGoogle = () => {
+    // Googleでログイン
+    signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem("isAuth", true);
+      setIsAuth(true);
+      navigate("/");
+    });
+  };
+  return (
+    <div>
+      <p>ログインして始める</p>
+      <button onClick={loginInWithGoogle}>Googleでログイン</button>
+    </div>
+  );
 };
