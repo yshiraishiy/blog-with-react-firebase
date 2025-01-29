86c68d0f80577d8d909761370f9dc3c9e1703c79
yshiraishiy <yuya0505soccer@gmail.com>
2025-01-29 17:11:48 +0900

    ✨firestoreからブログデータを取得

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
+import { useState } from "react";
 
 import "./Home.css";
 
 export const Home = () => {
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, "posts"));
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getPosts();
+  }, []);
+
   return (
     <div className="homePage">
-      <div className="postContents">
-        <div className="postHeader">
-          <h1>タイトル</h1>
-        </div>
-        <div className="postTextContainer">
-          今はReactの学習中です。 これから頑張って活躍していきたいです。
-          よろしくお願いします。
-        </div>
-        <div className="nameAndDeleteButton">
-          <h3>@白石裕也</h3>
-          <button>削除</button>
-        </div>
-      </div>
+      {postList.map((post) => {
+        return (
+          <div className="postContents" key={post.id}>
+            <div className="postHeader">
+              <h1>{post.title}</h1>
+            </div>
+            <div className="postTextContainer">{post.postsText}</div>
+            <div className="nameAndDeleteButton">
+              <h3>@{post.author.username}</h3>
+              <button>削除</button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
