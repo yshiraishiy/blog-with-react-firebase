d1d35923ab12b5c70536aa250273e58b05ebef01
yshiraishiy <yuya0505soccer@gmail.com>
2025-01-29 14:11:41 +0900

    ✨fontawesomeでアイコンを実装

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse, faFilePen, faArrowRightToBracket } from "@fortawesome/free-solid-svg-icons";
 
-import "./Navbar.css"
+import "./Navbar.css";
 
 export const Navbar = () => {
   return (
     <nav>
-      <Link to="/">ホーム</Link>
-      <Link to="/createpost">記事投稿</Link>
-      <Link to="/login">ログイン</Link>
+      <Link to="/"><FontAwesomeIcon icon={faHouse} />ホーム</Link>
+      <Link to="/createpost"><FontAwesomeIcon icon={faFilePen} />記事投稿</Link>
+      <Link to="/login"><FontAwesomeIcon icon={faArrowRightToBracket} />ログイン</Link>
     </nav>
   );
 };
